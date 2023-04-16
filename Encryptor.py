9e7b84427e27fb2c06d6f84e89e4add694cde593
vrglhuhu <cheanbvergel@gmail.com>
2023-04-16 15:22:31 +0800

    Making a header for this program.

--- Encryptor.py
+++ Encryptor.py
@@ -4,6 +4,19 @@
 
 # This program will encrypt a message using the Vigenère Cipher.
 # Create a header.
+import pyfiglet
+print("")
+print("")
+print("=" * 70)
+print("=" * 70)
+print("")
+welcome_one = pyfiglet.figlet_format("My Vigenère ".center(5, " "), font = "puffy" )
+print(welcome_one) 
+welcome_two = pyfiglet.figlet_format("Cipher".center(13, " "), font = "puffy" )
+print(welcome_two) 
+print("=" * 70)
+print("=" * 70)
+
 # Ask for the name of the user.
 # Ask user for the message and the key.
 # Ask the user if want to see the inputted message and key.
