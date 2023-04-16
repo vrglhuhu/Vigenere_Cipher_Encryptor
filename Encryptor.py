7c36c0189ad9a5b4c2ade4dae9857847cc9333a0
vrglhuhu <cheanbvergel@gmail.com>
2023-04-16 15:16:06 +0800

    My Psuedocode.

--- Encryptor.py
+++ Encryptor.py
@@ -1,3 +1,19 @@
 # Vergel, Chean Bernard Villanueva
 # Assignment No. 3
 # Question No. 3
+
+# This program will encrypt a message using the Vigenère Cipher.
+# Create a header.
+# Ask for the name of the user.
+# Ask user for the message and the key.
+# Ask the user if want to see the inputted message and key.
+# If the user answered yes print the message and key.
+   # Translate the key into corresponding letter values 0 - 25.
+   # Encrypt the message.
+   # Output the ciphertext.
+# Else if the user answered no.
+   # Translate the key into corresponding letter values 0 - 25.
+   # Encrypt the message.
+   # Output the ciphertext.
+# Else print invalid key.
+# Create a footer.
