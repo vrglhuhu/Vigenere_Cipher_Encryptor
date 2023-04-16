247372aa53cd4d5f3f15f0c0b682a72b48920067
vrglhuhu <cheanbvergel@gmail.com>
2023-04-16 16:41:19 +0800

    Adding some spaces.

--- Encryptor.py
+++ Encryptor.py
@@ -16,6 +16,7 @@ welcome_two = pyfiglet.figlet_format("Cipher".center(13, " "), font = "puffy" )
 print(welcome_two) 
 print("=" * 70)
 print("=" * 70)
+
 # Ask for the name of the user.
 print("Hi, I am Chean Bernard V. Vergel a first year college student at Polytechnic University of the Philippines.")
 print("")
@@ -23,6 +24,7 @@ name_of_user = input("How about you what is your name? ")
 print("")
 print("This program will encrypt a message using the Vigenère Cipher ")
 print("")
+
 # Ask user for the message and the key.
 print("FOR MESSAGE")
 print("")
@@ -34,8 +36,10 @@ print("")
 key = input("Please enter the key: ")
 key = key.upper().replace(" ", "")
 print("")
+
 # Ask the user if want to see the inputted message and key.
 agreement = str(input("Do you want to see your inputted mesessage and key? YES or NO: "))
+
 # If the user answered yes, print the message and key.
 if agreement.upper().replace(" ", "") == "YES":
     print("")
@@ -43,15 +47,19 @@ if agreement.upper().replace(" ", "") == "YES":
     print("")
     print("YOUR KEY IS: " + key)
     print("")
+
    # Translate the key into corresponding letter values 0 - 25.
     key_map = [ord(i) - 65 for i in key]
+
    # Encrypt the message.
     ciphertext = ""
     for i in range(len(message)):
         encrypted_num = (ord(message[i]) - 65 + key_map[i % len(key_map)]) % 26
         ciphertext += chr(encrypted_num + 65)
+
    # Output the ciphertext.
     print("The ciphertext is: " + ciphertext)
+
    # Create a footer.
     print("Thank you for your time")
     print("")
@@ -59,22 +67,28 @@ if agreement.upper().replace(" ", "") == "YES":
     goodbye = pyfiglet.figlet_format("Visit me again", font = "digital" )
     print (goodbye)
     print("") 
+
 # Else if the user answered no.
 elif agreement.upper().replace(" ", "") == "NO":
+
    # Translate the key into corresponding letter values 0 - 25.
     key_map = [ord(i) - 65 for i in key]
+
    # Encrypt the message.
     ciphertext = ""
     for i in range(len(message)):
         encrypted_num = (ord(message[i]) - 65 + key_map[i % len(key_map)]) % 26
         ciphertext += chr(encrypted_num + 65)
+
    # Output the ciphertext.
     print("The ciphertext is: " + ciphertext)
+
 # Else print invalid key.
     print("")
     print("Invalid key")
     print("")
     quit()
+    
 # Create a footer.
     print("Thank you for your time")
     print("")
